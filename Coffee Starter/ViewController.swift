399dc1ddbb97e3ba59fc14b82a7d0d7df50586ac
Michael Rockhold <michaelrockhold@users.noreply.github.com>
2019-05-01 17:28:02 -0700

    Initial Commit

---
+++ Coffee Starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee Starter
+//
+//  Created by Michael Rockhold on 5/1/19.
+//  Copyright © 2019 Michael Rockhold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
